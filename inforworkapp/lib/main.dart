c9b532ec6643a84ca8ad14850e670cb304a75c6b
Paul Ccuno <pallccuno@gmail.com>
2020-10-21 22:12:31 -0500

    Actualizacion pequeña

--- inforworkapp/lib/main.dart
+++ inforworkapp/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'INFOWORK',
       theme: ThemeData(
         textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
--- inforworkapp/lib/screens/empresa/empresa_screen.dart
+++ inforworkapp/lib/screens/empresa/empresa_screen.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:inforworkapp/constants.dart';
 
+import 'components/body.dart';
+
 class EmpresaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
       appBar: buildAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-      ),
+      body: Body(),
     );
   }
 
