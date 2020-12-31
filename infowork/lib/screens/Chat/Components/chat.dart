ab05f5d2127e630e8d7bec2ccd8d669d4db180b1
joselipa <joselipa123ochoa123@gmail.com>
2020-12-31 18:38:45 -0500

    List of messages

--- infowork/lib/screens/Chat/Components/chat.dart
+++ infowork/lib/screens/Chat/Components/chat.dart
@@ -1,21 +1,323 @@
 import 'package:flutter/material.dart';
-import 'package:infowork/constans.dart';
-import 'package:infowork/model/empresa.dart';
-import 'package:infowork/providers/empresa_provider.dart';
+import 'package:infowork/screens/help/components/help.dart';
+import 'package:infowork/screens/menu/components/body.dart';
+import 'package:infowork/screens/menu/components/maindraw.dart';
+
+import '../../../constans.dart';
+
+class User {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final bool isOnline;
+
+  User({
+    this.id,
+    this.name,
+    this.imageUrl,
+    this.isOnline,
+  });
+}
+
+// YOU - current user
+final User currentUser = User(
+  id: 0,
+  name: 'Nick Fury',
+  imageUrl: 'assets/images/nick-fury.jpg',
+  isOnline: true,
+);
+
+// USERS
+final User ironMan = User(
+  id: 1,
+  name: 'Iron Man',
+  imageUrl: 'assets/images/ironman.jpeg',
+  isOnline: true,
+);
+final User captainAmerica = User(
+  id: 2,
+  name: 'Captain America',
+  imageUrl: 'assets/images/captain-america.jpg',
+  isOnline: true,
+);
+final User hulk = User(
+  id: 3,
+  name: 'Hulk',
+  imageUrl: 'assets/images/hulk.jpg',
+  isOnline: false,
+);
+final User scarletWitch = User(
+  id: 4,
+  name: 'Scarlet Witch',
+  imageUrl: 'assets/images/scarlet-witch.jpg',
+  isOnline: false,
+);
+final User spiderMan = User(
+  id: 5,
+  name: 'Spider Man',
+  imageUrl: 'assets/images/spiderman.jpg',
+  isOnline: true,
+);
+final User blackWindow = User(
+  id: 6,
+  name: 'Black Widow',
+  imageUrl: 'assets/images/black-widow.jpg',
+  isOnline: false,
+);
+final User thor = User(
+  id: 7,
+  name: 'Thor',
+  imageUrl: 'assets/images/thor.png',
+  isOnline: false,
+);
+final User captainMarvel = User(
+  id: 8,
+  name: 'Captain Marvel',
+  imageUrl: 'assets/images/captain-marvel.jpg',
+  isOnline: false,
+);
+
+class Message {
+  final User sender;
+  final String
+      time; // Would usually be type DateTime or Firebase Timestamp in production apps
+  final String text;
+  final bool unread;
+
+  Message({
+    this.sender,
+    this.time,
+    this.text,
+    this.unread,
+  });
+}
+
+// EXAMPLE CHATS ON HOME SCREEN
+List<Message> chats = [
+  Message(
+    sender: ironMan,
+    time: '5:30 PM',
+    text: 'Hey dude! Even dead I\'m the hero. Love you 3000 guys.',
+    unread: true,
+  ),
+  Message(
+    sender: captainAmerica,
+    time: '4:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    unread: true,
+  ),
+  Message(
+    sender: blackWindow,
+    time: '3:30 PM',
+    text: 'WOW! this soul world is amazing, but miss you guys.',
+    unread: false,
+  ),
+  Message(
+    sender: spiderMan,
+    time: '2:30 PM',
+    text: 'I\'m exposed now. Please help me to hide my identity.',
+    unread: true,
+  ),
+  Message(
+    sender: hulk,
+    time: '1:30 PM',
+    text: 'HULK SMASH!!',
+    unread: false,
+  ),
+  Message(
+    sender: thor,
+    time: '12:30 PM',
+    text:
+        'I\'m hitting gym bro. I\'m immune to mortal deseases. Are you coming?',
+    unread: false,
+  ),
+  Message(
+    sender: scarletWitch,
+    time: '11:30 AM',
+    text: 'My twins are giving me headache. Give me some time please.',
+    unread: false,
+  ),
+  Message(
+    sender: captainMarvel,
+    time: '12:45 AM',
+    text: 'You\'re always special to me nick! But you know my struggle.',
+    unread: false,
+  ),
+];
+
+// EXAMPLE MESSAGES IN CHAT SCREEN
+List<Message> messages = [
+  Message(
+    sender: ironMan,
+    time: '5:30 PM',
+    text: 'Hey dude! Event dead I\'m the hero. Love you 3000 guys.',
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '4:30 PM',
+    text: 'We could surely handle this mess much easily if you were here.',
+    unread: true,
+  ),
+  Message(
+    sender: ironMan,
+    time: '3:45 PM',
+    text: 'Take care of peter. Give him all the protection & his aunt.',
+    unread: true,
+  ),
+  Message(
+    sender: ironMan,
+    time: '3:15 PM',
+    text: 'I\'m always proud of her and blessed to have both of them.',
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '2:30 PM',
+    text:
+        'But that spider kid is having some difficulties due his identity reveal by a blog called daily bugle.',
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '2:30 PM',
+    text:
+        'Pepper & Morgan is fine. They\'re strong as you. Morgan is a very brave girl, one day she\'ll make you proud.',
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '2:30 PM',
+    text: 'Yes Tony!',
+    unread: true,
+  ),
+  Message(
+    sender: ironMan,
+    time: '2:00 PM',
+    text: 'I hope my family is doing well.',
+    unread: true,
+  ),
+];
 
 class ChatScreen extends StatelessWidget {
-  final contratoProvider = new EmpresaProvider();
   final String empresa;
-
-  ChatScreen({Key key, this.empresa}) : super(key: key);
-
+  final String usuario;
+  const ChatScreen({Key key, this.empresa, this.usuario}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("asd"),
+      backgroundColor: kPrimaryColor,
+      appBar: buildAppBar(),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      color: kBackgroundColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                  ),
+                  ListView.builder(
+                      itemCount: chats.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final Message chat = chats[index];
+                        return Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 15),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                padding: EdgeInsets.all(2),
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(40)),
+                                  border: Border.all(
+                                    width: 2,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 2,
+                                      blurRadius: 5,
+                                    )
+                                  ],
+                                ),
+                                child: CircleAvatar(
+                                  radius: 35,
+                                  backgroundImage: AssetImage(
+                                      'assets/images/trabajador.png'),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.65,
+                                padding: EdgeInsets.only(left: 20),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text(
+                                          chat.sender.name,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          chat.time,
+                                          style: TextStyle(
+                                            fontSize: 11,
+                                            fontWeight: FontWeight.w300,
+                                            color: Colors.black54,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Container(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        chat.text,
+                                        style: TextStyle(
+                                          fontSize: 13,
+                                          color: Colors.black54,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      })
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
-
-
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      title: Text("Reclamos"),
+      centerTitle: true,
+      toolbarHeight: 60,
+    );
+  }
 }
