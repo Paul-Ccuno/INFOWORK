8c5011f3d4dea5692fb9cf4653cd0a07380ce90d
joselipa <joselipa123ochoa123@gmail.com>
2021-01-12 15:25:46 -0500

    Intento de pago

---
+++ infowork/lib/utils/globals.dart
@@ -0,0 +1,11 @@
+library my_prj.globals;
+
+String mpPublicKey =
+    "pk_test_51I66VVLh36qDsj0JP1ZPofOY5UZI5xgdd2fraYEcfzBMCKXv843B6BhAuZ12LT2DPL43x33WcbVkdVq1fXTgNkb300FPUAUwZH";
+String mpAccessToken =
+    "APP_USR-2334006277803107-011104-9bf7694114ff65d00d62428e03a57e79-699927963";
+String mpTestPublicKey = "TEST-2e1928e0-a0b8-4f6d-92e3-7f8e9c6a35fd";
+String mpTestAccessToken =
+    "TEST-2334006277803107-011104-a60e70b57b48b426236af0b0af8a6793-699927963";
+String mpClientID = "2334006277803107";
+String mpClientSecret = "ngIVH4HFIxaqqJqtGZNPVywLnUgjT6wd";
